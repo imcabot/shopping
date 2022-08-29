a3074144f3bfe1b15228f518f065d0753112209b
imcabot <xwchen0@163.com>
2022-08-30 06:35:41 +0800

    创建商品模型和自定义错误

---
+++ domain/product/entity.go
@@ -0,0 +1,30 @@
+package product
+
+import (
+	"gorm.io/gorm"
+	"shoping/category"
+)
+
+//商品结构体
+type Product struct {
+	gorm.Model
+	Name       string
+	SKU        string
+	Desc       string
+	StockCount int
+	Price      float32
+	CategoryID uint
+	Category   category.Category `json:"-"`
+	IsDeleted  bool
+}
+
+func NewProduct(name string, desc string, stockCount int, price float32, cid uint) *Product {
+	return &Product{
+		Name:       name,
+		Desc:       desc,
+		StockCount: stockCount,
+		Price:      price,
+		CategoryID: cid,
+		IsDeleted:  false,
+	}
+}
